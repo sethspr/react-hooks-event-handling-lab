1e8a72a6c359284acd2c630e9adb5a6e7c29e5dc
seth spring <seth.c.spring@gmail.com>
2024-02-23 14:51:46 -0500

    keypad eyesOnMe event handling complete

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import EyesOnMe from "./EyesOnMe";
+import Keypad from "./Keypad";
+
+
+function App() {
+    return (
+      <div className="App">
+        <Keypad />
+        <EyesOnMe />
+      </div>
+    );
+  }
+  
+  export default App;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
 // Code Keypad Component Here
+import React from 'react'
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handlePassword() {
+    console.log("Entering password...");
+  }
+
+  return (
+    <div>
+      <input
+        onChange={handlePassword}
+        type="password"
+        name="new-password"
+        text="Entering Password..."
+      />
+    </div>
+  );
 }
 
 export default Keypad;
